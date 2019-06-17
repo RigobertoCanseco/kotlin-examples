0795a2ebcd0e361f8d068a8ed6a9940050402847
Rigoberto Canseco <cansecorigoberto@gmail.com>
2019-06-17 09:10:54 -0500

    data types

--- src/Greeter.kt
+++ src/Greeter.kt
@@ -1,9 +1,10 @@
 class Greeter{
-    private var intVar: Int = 0;
-    private var stringVar: String = "";
-    private var doubleVar: Double = .0;
-    private var charVar: Char = ' ';
-    private var booleanvar: Boolean = !false;
+    private var intVar: Int = 0
+    private var stringVar: String = ""
+    private var doubleVar: Double = .0
+    private var charVar: Char = ' '
+    private var booleanvar: Boolean = !false
+    private var name: String? = null
 
     constructor(intVar: Int, stringVar: String, doubleVar: Double, charVar: Char, booleanvar: Boolean) {
         this.intVar = intVar
@@ -13,31 +14,55 @@ class Greeter{
         this.booleanvar = booleanvar
     }
 
-    constructor()
+    constructor(name: String){
+        this.name = name
+    }
 
-    fun getStringVar(): String{
-        return this.stringVar;
+
+
+    infix fun say(msg: String) = println("$name is saying $msg")
+    
+    infix operator fun plus(greeter: Greeter) : Greeter {
+        this.name += "${greeter.name}"
+        
+        return this
+    }
+
+
+    infix operator fun times (greeter: Greeter) : Greeter {
+        this.name += "${greeter.name}"
+
+        return this
+    }
+
+
+    fun say() = println("$name")
+
+    fun getStringVar(): Any{
+        var finally = fun(): Int{
+            return 0
+        }
+
+        return finally()
     }
 
     fun greet(name: String): String{
-        var rem = 1;
-        var a = 0;
-        var b = 0;
+        var rem = 1
+        var a = 0
+        var b = 0
         while (rem != 0) {
             try {
                 rem = a % b
                 a = b
                 b = rem
             }catch (e:Exception){
-                System.out.println(e.stackTrace);
-                break;
+                System.out.println(e.stackTrace)
+                break
             }
         }
-
-        println(b);
-        return "Hello $name !!";
-
+        return "Hello $name !!"
     }
+
 
     override fun toString(): String {
         return "Greeter(intVar=$intVar, stringVar='$stringVar', doubleVar=$doubleVar, charVar=$charVar, booleanvar=$booleanvar)"
--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,52 @@
 fun main(args: Array<String> ){
-    var greter: Greeter = Greeter();
-    print(greter.greet("Rigoberto"));
+//    var greter: Greeter = Greeter("Juan");
+//    println(greter.greet("Rigoberto"));
+//    println(greter.getStringVar());
+//
+//    greter say "Hola";
+//    greter.say();
+//
+
+//    var g1 = Greeter("Juan");
+//    var g2 = Greeter("Luis");
+//
+//    var g3 = g1 + g2;
+//
+//    g3.say()
+//
+//   g3 = g1 plus g2;
+//    g3.say()
+
+    var child: Child = Child()
+    child.foo()
+}
+
+
+interface A {
+    fun foo() {
+        println("A:foo")
+    }
+
+}
+interface B {
+    fun foo() {
+        println("B:foo")
+    }
+}
+class Child : A, B {
+    override fun foo () {
+        super<A>.foo();
+        super<B>.foo();
+
+        println("Child:foo")
+    }
+
+    constructor(){
+        println("constructor")
+    }
+
+    init {
+        println("init");
+    }
+
 }
